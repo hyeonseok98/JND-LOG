4c3e7c4b77eab9062c693b682f1cd4ea608f8c45
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2025-07-16 12:17:14 +0900

    🚑 hotfix: axios instance 수정_2

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,21 +1,13 @@
 import axios from "axios";
 
-const isServer = typeof window === "undefined";
-
-function getBaseURL() {
-  if (isServer) {
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-
-    return "http://localhost:3000";
-  }
-
-  return "";
-}
+export const API_BASE_URL = process.env.NEXT_PUBLIC_LOCAL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
 
 export const apiClient = axios.create({
-  baseURL: getBaseURL(),
-  headers: { "Content-Type": "application/json" },
-  timeout: 15_000,
+  baseURL: API_BASE_URL,
+  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
